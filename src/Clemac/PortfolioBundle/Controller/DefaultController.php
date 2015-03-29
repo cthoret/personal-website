d57d153b4c65e1ef663b47d43c63e2edcbba12db
Clement Thoret <clement.thoret@gmail.com>
2015-03-29 03:53:58 +0200

    creat filst controlleur

--- src/Clemac/PortfolioBundle/Controller/DefaultController.php
+++ src/Clemac/PortfolioBundle/Controller/DefaultController.php
@@ -9,11 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/", name="clemac_portfolio_front_home")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return $this->render('ClemacPortfolioBundle:Front/Default:home.html.twig', [
+        ]);
     }
 }
